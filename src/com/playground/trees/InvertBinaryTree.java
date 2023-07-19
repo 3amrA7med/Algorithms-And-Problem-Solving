7c2f30fa3e1146ff6d1e98252a1d6e810dda369e
3amrA7med <amr.ahmed.abdelbaqi@gmail.com>
2023-07-19 16:57:02 +0300

    Added problem "226. Invert Binary Tree" solution.

---
+++ src/com/playground/trees/InvertBinaryTree.java
@@ -0,0 +1,23 @@
+package com.playground.trees;
+
+import com.playground.trees.model.TreeNode;
+
+/**
+ * 226. Invert Binary Tree
+ */
+public class InvertBinaryTree {
+
+    // 100% runtime, 80% memory
+
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+}
